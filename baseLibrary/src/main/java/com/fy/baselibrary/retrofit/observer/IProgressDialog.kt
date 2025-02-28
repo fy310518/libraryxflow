57547a58de6156905700f1068ed661ead6cf36cf
fy310518 <fy310518@163.com>
2025-02-28 16:58:01 +0800

    test

---
+++ baseLibrary/src/main/java/com/fy/baselibrary/retrofit/observer/IProgressDialog.kt
@@ -0,0 +1,82 @@
+package com.fy.baselibrary.retrofit.observer
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.fy.baselibrary.base.dialog.CommonDialog
+import com.fy.baselibrary.retrofit.NetAnimListener
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+
+/**
+ * 自定义对话框的dialog
+ * Created by fangs on 2017/11/7.
+ */
+class IProgressDialog() {
+    protected var mContext: Context? = null
+
+    /** 传递进来的 环境（AppCompatActivity or Fragment）  */
+    var obj: Any? = null
+
+    var mDialog: CommonDialog<*, *>? = null
+
+    protected var netAnimListener: NetAnimListener? = null
+
+    protected var scope: CoroutineScope? = null
+
+
+
+    constructor(obj: Any, netAnimListener: NetAnimListener, scope: CoroutineScope? = null): this() {
+        this.obj = obj
+        this.netAnimListener = netAnimListener
+        this.scope = scope
+    }
+
+    constructor(obj: Any, dialog: CommonDialog<*, *>, scope: CoroutineScope? = null): this() {
+        this.obj = obj
+        this.mDialog = dialog
+        this.scope = scope
+    }
+
+
+    private fun runShowDialog(manager: FragmentManager){
+        mDialog?.apply {
+            if((null == dialog || dialog!!.isShowing)){
+                setDialogList{
+                    scope?.cancel()
+                }
+                show(manager, javaClass.name)
+            }
+        }
+    }
+
+    /**
+     * 显示对话框
+     */
+    fun show() {
+        obj?.apply {
+            when (this) {
+                is AppCompatActivity -> {
+                    mContext = obj as AppCompatActivity
+                    runShowDialog(supportFragmentManager)
+                }
+                is Fragment -> {
+                    mContext = (obj as Fragment).context
+                    runShowDialog(childFragmentManager)
+                }
+            }
+        }
+    }
+
+    /**
+     * 关闭对话框
+     */
+    fun close() {
+        if (null != mDialog && null != mContext) {
+            mDialog?.dismiss(false)
+        } else if (null != netAnimListener && null != mContext) {
+            netAnimListener?.stop()
+        }
+    }
+}
